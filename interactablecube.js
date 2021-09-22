c3c850f64b568d16bfce40e43d853faf4aa87d80
racheldepaz <rxd678@miami.edu>
2021-09-22 13:37:44 -0400

    trying to fix git lfs .html file issues

--- interactablecube.js
+++ interactablecube.js
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ce08943759c730a3c544045dc39528ead19660f3d7edbec385046a2de2db2e9f
-size 332
+AFRAME.registerComponent('scale-on-mouseenter', {
+    schema: {
+      to: {default: '2.5 2.5 2.5', type: 'vec3'}
+    },
+
+    init: function () {
+      var data = this.data;
+      var el = this.el;
+      this.el.addEventListener('mouseenter', function () {
+        el.object3D.scale.copy(data.to);
+      });
+    }
+  });
