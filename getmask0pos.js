6194f8a9a1d1c17ca1ee888b27d514d51ec342a8
racheldepaz <rxd678@miami.edu>
2021-10-26 15:29:01 -0400

    redirect when aframe component works

--- getmask0pos.js
+++ getmask0pos.js
@@ -9,6 +9,6 @@ AFRAME.registerComponent("mask-0", {
         // your code here - the scene should be ready
         var marker0 = document.querySelector("marker-0");
         var marker0_pos = marker0.getAttribute("position");
-        console.log("marker_0 pos: " + marker0_pos);
+        window.location.href = "test.html";
     },
 });
