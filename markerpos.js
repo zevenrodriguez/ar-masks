89903c77c21a2df90367b0be4cd1a167a9a14ef1
racheldepaz <rxd678@miami.edu>
2021-11-02 14:51:20 -0400

    trying to add mask 2 and mask 3

--- markerpos.js
+++ markerpos.js
@@ -12,7 +12,7 @@ AFRAME.registerComponent('marker0handler', {
 
     tick: function() {
         var data = this.data;
-        this.el.sceneEl.addEventListener('markerFound', () => {
+        this.el.sceneEl.querySelector("#mask0").addEventListener('markerFound', () => {
             this.el.object3D.visible = true; //set object visible when first tracked //TODO on click, hide model
             markerPosition[data.id] = this.el.object3D.position; //set marker position to world position
 
@@ -35,7 +35,29 @@ AFRAME.registerComponent('marker1handler', {
 
     tick: function() {
         var data = this.data;
-        this.el.sceneEl.addEventListener('markerFound', () => {
+        this.el.sceneEl.querySelector("#mask1").addEventListener('markerFound', () => {
+            this.el.object3D.visible = true; //set object visible when first tracked //TODO on click, hide model
+            markerPosition[data.id] = this.el.object3D.position; //set marker position to world position
+        })
+
+        //glb elements are updated every frame
+        document.getElementById('m' + data.id).object3D.position.set(markerPosition[data.id].x, markerPosition[data.id].y, markerPosition[data.id].z); //TODO lerp 
+    }
+});
+
+AFRAME.registerComponent('marker2handler', {
+    schema: {
+        id: { type: 'int', default: -1 }
+    },
+
+    init: function() {
+        this.el.object3D.visible = "false";
+
+    },
+
+    tick: function() {
+        var data = this.data;
+        this.el.sceneEl.querySelector('#mask2').addEventListener('markerFound', () => {
             this.el.object3D.visible = true; //set object visible when first tracked //TODO on click, hide model
             markerPosition[data.id] = this.el.object3D.position; //set marker position to world position
         })
